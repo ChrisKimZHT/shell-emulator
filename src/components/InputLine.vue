97d5cd841e6e765cc973e2f623a53853a8b392b3
ChrisKim <1515748548@qq.com>
2023-12-21 23:17:24 +0800

    [feat] 历史记录功能

--- src/components/InputLine.vue
+++ src/components/InputLine.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="input-line">
     <span class="shell-prompt">{{ getShellPrompt() }}</span>
-    <span class="input-area" ref="input-area" contenteditable="true" @input="updateCommand">
+    <span class="input-area" ref="inputArea" contenteditable="true" @input="updateCommand">
       {{ currentCommand }}
     </span>
   </div>
@@ -20,11 +20,13 @@ export default {
   data() {
     return {
       currentCommand: "",
+      historyCommands: [],
+      curHistoryIndex: 0,
     }
   },
   methods: {
     getFocus() {
-      this.$refs["input-area"].focus()
+      this.$refs.inputArea.focus()
     },
     getShellPrompt() {
       let promptDir = "";
@@ -38,25 +40,47 @@ export default {
     },
     updateCommand(event) {
       this.currentCommand = event.target.innerText;
+      this.curHistoryIndex = this.historyCommands.length;
     },
     finishedInput() {
       this.$emit("finished-input", this.getShellPrompt(), this.currentCommand);
+      this.historyCommands.push(this.currentCommand);
+      this.curHistoryIndex = this.historyCommands.length;
       this.currentCommand = "";
     },
     interruptInput() {
       this.$emit("interrupt-input", this.getShellPrompt(), this.currentCommand);
       this.currentCommand = "";
     },
+    onPrevHistory() {
+      if (this.curHistoryIndex > 0) {
+        this.curHistoryIndex--;
+        this.currentCommand = this.historyCommands[this.curHistoryIndex];
+      }
+    },
+    onNextHistory() {
+      if (this.curHistoryIndex < this.historyCommands.length - 1) {
+        this.curHistoryIndex++;
+        this.currentCommand = this.historyCommands[this.curHistoryIndex];
+      } else {
+        this.curHistoryIndex = this.historyCommands.length;
+        this.currentCommand = "";
+      }
+    }
   },
   mounted() {
-    eventBus.on("key-down", this.getFocus)
+    eventBus.on("key-down", this.getFocus);
     eventBus.on("ctrl-c", this.interruptInput);
     eventBus.on("enter", this.finishedInput);
+    eventBus.on("arrow-up", this.onPrevHistory);
+    eventBus.on("arrow-down", this.onNextHistory);
   },
   unmounted() {
-    eventBus.off("key-down", this.getFocus)
+    eventBus.off("key-down", this.getFocus);
     eventBus.off("ctrl-c", this.interruptInput);
     eventBus.off("enter", this.finishedInput);
+    eventBus.off("arrow-up", this.onPrevHistory);
+    eventBus.off("arrow-down", this.onNextHistory);
   },
 }
 </script>
