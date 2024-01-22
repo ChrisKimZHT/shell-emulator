797400c2ae986623e04e54c81c2e2b616cf72a10
ChrisKim <1515748548@qq.com>
2024-01-22 21:43:46 +0800

    [feat] 随机ciallo

--- src/commands/ciallo.js
+++ src/commands/ciallo.js
@@ -1,24 +1,25 @@
 /* Ciallo～(∠・ω< )⌒★ */
+const cialloType = ["Ciallo～(∠・ω< )⌒☆", "Ciallo～(∠・ω< )⌒★"]
+const colorType = ["aqua", "coral", "red", "greenyellow", "pink", "green", "blue", "yellow", "orange", "gray"]
+const scrollamountRange = [8, 32]
+const sizeRange = [5, 20]
+
+function randomOne() {
+  const ciallo = cialloType[Math.floor(Math.random() * cialloType.length)]
+  const color = colorType[Math.floor(Math.random() * colorType.length)]
+  const scrollamount = Math.floor(Math.random() * (scrollamountRange[1] - scrollamountRange[0])) + scrollamountRange[0]
+  const size = Math.floor(Math.random() * (sizeRange[1] - sizeRange[0])) + sizeRange[0]
+  return `<marquee behavior="scroll" direction="right" scrollamount="${scrollamount}" style="line-height: normal;"><font color="${color}" size="${size}px">${ciallo}</font></marquee>`
+}
+
 export default function ciallo(cwd, args) {
   if (args.length > 0) {
     return "ciallo: too many arguments";
   }
-  return [
-    "idea from <a href=\"https://ciallo.cc/\">ciallo.cc</a>.",
-    "<marquee behavior=\"scroll\" direction=\"right\" scrollamount=\"15\" style=\"line-height: normal;\"><font color=\"aqua\" size=\"11px\">Ciallo～(∠・ω< )⌒☆</font></marquee>",
-    "<marquee behavior=\"scroll\" direction=\"right\" scrollamount=\"12\" style=\"line-height: normal;\"><font color=\"coral\" size=\"10px\">Ciallo～(∠・ω< )⌒★</font></marquee>",
-    "<marquee behavior=\"scroll\" direction=\"right\" scrollamount=\"30\" style=\"line-height: normal;\"><font color=\"red\" size=\"15px\">Ciallo～(∠・ω< )⌒★</font></marquee>",
-    "<marquee behavior=\"scroll\" direction=\"right\" scrollamount=\"20\" style=\"line-height: normal;\"><font color=\"greenyellow\" size=\"13px\">Ciallo～(∠・ω< )⌒☆</font></marquee>",
-    "<marquee behavior=\"scroll\" direction=\"right\" scrollamount=\"15\" style=\"line-height: normal;\"><font color=\"pink\" size=\"20px\">Ciallo～(∠・ω< )⌒★</font></marquee>",
-    "<marquee behavior=\"scroll\" direction=\"right\" scrollamount=\"11\" style=\"line-height: normal;\"><font color=\"aqua\" size=\"6px\">Ciallo～(∠・ω< )⌒☆</font></marquee>",
-    "<marquee behavior=\"scroll\" direction=\"right\" scrollamount=\"8\" style=\"line-height: normal;\"><font color=\"greenyellow\" size=\"10px\">Ciallo～(∠・ω< )⌒★</font></marquee>",
-    "<marquee behavior=\"scroll\" direction=\"right\" scrollamount=\"20\" style=\"line-height: normal;\"><font color=\"pink\" size=\"9px\">Ciallo～(∠・ω< )⌒★</font></marquee>",
-    "<marquee behavior=\"scroll\" direction=\"right\" scrollamount=\"28\" style=\"line-height: normal;\"><font color=\"red\">Ciallo～(∠・ω< )⌒☆</font></marquee>",
-    "<marquee behavior=\"scroll\" direction=\"right\" scrollamount=\"13\" style=\"line-height: normal;\"><font color=\"green\" size=\"5px\">Ciallo～(∠・ω< )⌒☆</font></marquee>",
-    "<marquee behavior=\"scroll\" direction=\"right\" scrollamount=\"17\" style=\"line-height: normal;\"><font color=\"blue\" size=\"5px\">Ciallo～(∠・ω< )⌒☆</font></marquee>",
-    "<marquee behavior=\"scroll\" direction=\"right\" scrollamount=\"16\" style=\"line-height: normal;\"><font color=\"yellow\" size=\"7px\">Ciallo～(∠・ω< )⌒★</font></marquee>",
-    "<marquee behavior=\"scroll\" direction=\"right\" scrollamount=\"10\" style=\"line-height: normal;\"><font color=\"orange\" size=\"8px\">Ciallo～(∠・ω< )⌒☆</font></marquee>",
-    "<marquee behavior=\"scroll\" direction=\"right\" scrollamount=\"20\" style=\"line-height: normal;\"><font color=\"gray\" size=\"9px\">Ciallo～(∠・ω< )⌒★</font></marquee>",
-  ].join("");
+  const result = ["idea from <a href=\"https://ciallo.cc/\">ciallo.cc</a>."];
+  for (let i = 0; i < 10; i++) {
+    result.push(randomOne());
+  }
+  return result.join("");
 }
 /* Ciallo～(∠・ω< )⌒★ */ 
