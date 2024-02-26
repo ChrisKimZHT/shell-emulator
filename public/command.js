5a4fea3561e7bde44f285cab52d8c788f1a68552
ChrisKim <1515748548@qq.com>
2024-02-26 14:39:17 +0800

    [refactor] 删除一些非核心内容

--- public/command.js
+++ public/command.js
@@ -1,10 +1,8 @@
 // 1. 首先在cmd目录下创建你的命令，实现命令函数，可选实现提示函数。
 
 // 2. 然后在这里import导入你的命令函数以及可选的提示函数。
-import ciallo from "./cmd/ciallo.js";
 import debug from "./cmd/debug.js";
 import echo from "./cmd/echo.js";
-import hello from "./cmd/hello.js";
 import help from "./cmd/help.js";
 import pwd from "./cmd/pwd.js";
 import reboot from "./cmd/reboot.js";
@@ -14,10 +12,8 @@ import uptime from "./cmd/uptime.js";
 
 // 3. 最后将你的命令函数和可选的提示函数注册到列表中。
 window.externalCommand = [
-  { name: "ciallo", func: ciallo },
   { name: "debug", func: debug },
   { name: "echo", func: echo },
-  { name: "hello", func: hello },
   { name: "help", func: help },
   { name: "pwd", func: pwd },
   { name: "reboot", func: reboot },
